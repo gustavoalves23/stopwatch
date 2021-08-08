5492f96a13dbd1a63ab7e5800de8b6ac00708dfa
gustavoalves23 <gustavo.alves388@gmail.com>
2021-08-08 17:51:46 -0300

    adição do contador

--- src/Pages/TimerPage.jsx
+++ src/Pages/TimerPage.jsx
@@ -4,24 +4,51 @@ class TimerPage extends React.Component {
   constructor(props) {
     super(props);
     const { value } = this.props;
+    const minutes = (Number(value.substring(0,2)) > 9) ? Number(value.substring(0,2)) : "0" + Number(value.substring(0,2));
+    const seconds = (Number(value.substring(2,4)) > 9) ? Number(value.substring(2,4)) : "0" + Number(value.substring(2,4));
+    console.log((Number(value.substring(0,2)) * 60) + Number(value.substring(2,4)));
     this.state = {
-      minute1: Number(value.substring(0,1)),
-      minute2: Number(value.substring(1,2)),
-      second1: Number(value.substring(2,3)),
-      second2: Number(value.substring(3,4)),
+      minutes,
+      seconds,
+      time: (Number(value.substring(0,2)) * 60) + Number(value.substring(2,4)),
+    }
+  }
+
+  timer = () => {
+    this.setState((previousState) => ({
+      time: previousState.time - 1,
+      minutes: (Math.floor(previousState.time / 60) > 9) ? Math.floor(previousState.time / 60) : "0" + Math.floor(previousState.time / 60), 
+      seconds: (previousState.time % 60 > 9) ? previousState.time % 60 : "0" + (previousState.time % 60) ,
+    }))
+  }
+
+  startTimer = () => {
+    this.countdown = setInterval(this.timer, 1000);
+  }
+
+  stopTimer = () => {
+    clearInterval(this.countdown);
+  }
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  componentDidUpdate() {
+    if (this.state.time === - 1) {
+      this.stopTimer();
     }
   }
 
   render() {
-    const {minute1, minute2, second1, second2 } = this.state;
+    const {minutes, seconds } = this.state;
     return (
-      <div>
-        <h1>
-          <span>{ minute1 }</span>
-          <span>{ minute2 }</span>
-          <span>{ second1 }</span>
-          <span>{ second2 }</span>
-        </h1>
+      <div className="timer-page">
+        <div className="timer">
+          <div className="timerdiv">{ minutes }</div>
+          <div className="colondiv" >:</div>
+          <div className="timerdiv">{ seconds }</div>
+        </div>
       </div>
     )
   }
